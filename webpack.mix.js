db1247fec8c5ada7f835afecfb4c59e323a37f3b
Ernest Walzel <ewalzel@gmail.com>
2021-01-28 08:40:25 +0100

    Mix now supports Tailwind properly

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,14 +14,10 @@ const tailwindPlugin =  require('tailwindcss')
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
         tailwindPlugin({ config: './tailwind.config.js' }), // Explicit default
-        require('autoprefixer'),
     ])
     .postCss('resources/css/admin.css', 'public/css', [
-        require('postcss-import'),
         tailwindPlugin({ config: './tailwind-admin.config.js' }),
-        require('autoprefixer'),
     ]);
 
 if (mix.inProduction()) {
