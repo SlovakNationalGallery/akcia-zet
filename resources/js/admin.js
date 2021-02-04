283afc4df7e26e066b3a269d2f03bf16d7aecdad
Ernest Walzel <ewalzel@gmail.com>
2021-02-04 12:04:51 +0100

    Add Quill editor

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -2,3 +2,21 @@ require('./bootstrap');
 
 require('alpinejs');
 require('./trix');
+const Quill = require('quill');
+
+function initQuill(ref, dispatch) {
+    const toolbar = [
+        ['bold', 'italic'],        // toggled buttons
+        ['blockquote'],
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        ['clean'],                                         // remove formatting button
+        ['link', 'image', 'video']
+    ]
+
+    const quill = new Quill(ref, {theme: 'snow', modules: { toolbar } });
+    quill.on('text-change', function () {
+        dispatch('text-change', { content: quill.root.innerHTML })
+    });
+}
+
+window.initQuill = initQuill
