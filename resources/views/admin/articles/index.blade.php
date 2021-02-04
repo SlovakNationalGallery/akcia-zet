e5ef4339382d308ac3955237fab7d7bc15485c85
Ernest Walzel <ewalzel@gmail.com>
2021-02-04 14:06:17 +0100

    No image for article index (yet)

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -20,11 +20,11 @@
                                         <tr>
                                             <td class="px-6 py-4 whitespace-nowrap w-2/3">
                                                 <div class="flex items-start">
-                                                    <div class="flex-shrink-0 h-20">
+                                                    {{-- <div class="flex-shrink-0 h-20">
                                                         <img class="h-20"
                                                             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
                                                             alt="">
-                                                    </div>
+                                                    </div> --}}
                                                     <div class="ml-4">
                                                         <div class="text-md font-medium text-gray-900">
                                                             {{ $article->title }}
