27a61cf9b01e92ad032ae866957d1d869d1892dd
Ernest Walzel <ewalzel@gmail.com>
2021-02-11 10:06:51 +0100

    welcome -> welcome-old

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome-old');
 });
 
 Route::prefix('admin')->name('admin.')->middleware(['auth:sanctum', 'verified'])->group(function () {
