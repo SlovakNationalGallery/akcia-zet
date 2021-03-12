00c9e7203c5b168f55258cee8421bbc3058352e0
Ernest Walzel <ewalzel@gmail.com>
2021-03-12 11:57:03 +0100

    Working links from article preview

--- resources/views/components/article-preview.blade.php
+++ resources/views/components/article-preview.blade.php
@@ -1,9 +1,5 @@
 @props(['article'])
 
-@php
-    $article = $article ?? \App\Models\Article::first();
-@endphp
-
 <div {{ $attributes }}>
     <p class="text-gray-400 text-sm">
         {{ ucfirst($article->published_at->diffForHumans()) }}
@@ -14,14 +10,14 @@
             <x-extended-embed url="{{ $article->embed_url }}" />
         </div>
         @endif
-        <h3 class="text-lg text-yellow-200 slab {{ $article->embed_url ? 'md:text-left' : ''}}"><a href="#TODO">{{ $article->title }}</a></h3>
+        <h3 class="text-lg text-yellow-200 slab {{ $article->embed_url ? 'md:text-left' : ''}}"><a href="{{ route('articles.show', $article)}}">{{ $article->title }}</a></h3>
     </div>
     <p class="text-white text-sm mt-2">
         {{ $article->perex }}
     </p>
     <div class="mt-2 text-sm">
         @foreach($article->tags as $tag)
-            <a href="#" class="text-pink-400 mr-1">#{{ $tag->name }}</a>
+            <a href="{{ route('articles.index', ['tag' => $tag->name]) }}" class="text-pink-400 mr-1">#{{ $tag->name }}</a>
         @endforeach
     </div>
 </div>
