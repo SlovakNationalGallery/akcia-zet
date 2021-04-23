96b7f0fc7df6125a748761c34b7dd71e37cc1dac
Ernest Walzel <ewalzel@gmail.com>
2021-04-23 10:00:01 +0200

    Styling tweaks

--- resources/views/livewire/admin/articles/form.blade.php
+++ resources/views/livewire/admin/articles/form.blade.php
@@ -60,7 +60,13 @@
                 </div>
 
                 <div class="col-span-6 sm:col-span-4">
-                    <x-jet-label for="attachments" value="Prílohy" class="font-bold text-xl" />
+                    <x-jet-label for="attachments" class="font-bold text-xl">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 -mt-1 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                        </svg>
+                        Prílohy
+                    </x-jet-label>
+
                     <x-admin.input-hint>Súbory, na ktoré sa dá odkazovať v článku.<br/> (Pravý klik na odkaz "Download" -> skopírovať odkaz)</x-admin.input-hint>
                     <div class="mt-1">
                         <x-media-library-collection
