203165747cae283ec46519b86df37f64cdda3a30
Ernest Walzel <ewalzel@gmail.com>
2021-04-23 13:51:31 +0200

    Tweak Issuu embeds

--- resources/views/vendor/embed/services/issuu.blade.php
+++ resources/views/vendor/embed/services/issuu.blade.php
@@ -1,5 +1,5 @@
 <iframe
-    src="https://e.issuu.com/issuu-reader3-embed-files/latest/document-page-reader.html?u={{ $user }}&d={{ $document }}&p=1"
+    src="//e.issuu.com/embed.html?u={{ $user }}&d={{ $document }}"
     width="100%"
     height="326px"
     frameborder="0"
