8ac6687e50c3be7946a8c6fb3a96a498e030a5e7
Ernest Walzel <ewalzel@gmail.com>
2021-04-23 13:16:48 +0200

    Remove &r from issuu embed

--- resources/views/vendor/embed/services/issuu.blade.php
+++ resources/views/vendor/embed/services/issuu.blade.php
@@ -1,5 +1,5 @@
 <iframe
-    src="https://e.issuu.com/issuu-reader3-embed-files/latest/document-page-reader.html?u={{ $user }}&d={{ $document }}&p=1&r=https%3A%2F%2Fissuu.com"
+    src="https://e.issuu.com/issuu-reader3-embed-files/latest/document-page-reader.html?u={{ $user }}&d={{ $document }}&p=1"
     width="100%"
     height="326px"
     frameborder="0"
