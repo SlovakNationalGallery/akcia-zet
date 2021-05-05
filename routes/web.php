246c40fa1f09272e669dff3e2ebdca23110a2934
Ernest Walzel <ewalzel@gmail.com>
2021-05-05 12:51:37 +0200

    Publish content pages

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,78 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome-old');
-});
+    $articles = Article::published()->orderBy('published_at', 'desc')->take(4)->get();
+    $featuredArticle = $articles->shift();
+
+    $timelapseImages = Setting::first()->getMedia('timelapse');
+    $timelapseImagesDates = $timelapseImages
+        ->map(fn ($image) => Carbon::parse($image->getCustomProperty('date')));
+
+    $researchArticles = Article::whereIn('slug', ['ztraceny-svaty', 'z-vyskumu-obrazu'])->get();
+
+    return view('welcome', compact('articles', 'featuredArticle', 'timelapseImages', 'timelapseImagesDates', 'researchArticles'));
+})->name('home');
+
+Route::get('/akteri', function () {
+    $articles = Article::published()->orderBy('published_at', 'desc')->take(2)->get();
+    return view('actors', compact('articles'));
+})->name('actors');
+
+Route::get('/pridane', function (Request $request) {
+    $articles = Article::published()->with('tags')->orderBy('published_at', 'desc');
+    $tag = $request->get('tag');
+
+    if ($tag) $articles = $articles->withAnyTags($tag);
+    $articles = $articles->get();
+
+    $comingSoon = Setting::first()->coming_soon;
+
+    return view('articles.index', compact('articles', 'tag', 'comingSoon'));
+})->name('articles.index');
+
+Route::get('/pridane/{article:slug}', function (Article $article) {
+    $articles = Article::published()->orderBy('published_at')->get();
+    $articleIndex = $articleIndex = $articles->search(fn ($haystack) => $haystack->id == $article->id);
+
+    // Wrap-around next and prev articles
+    $nextArticle = $articles
+        ->slice($articleIndex + 1)
+        ->first()
+        ??
+        $articles->first();
+
+    $prevArticle = $articles
+        ->slice(0, $articleIndex)
+        ->last()
+        ??
+        $articles->last();
+
+
+    return view('articles.show', compact('article', 'nextArticle', 'prevArticle'));
+})->name('articles.show');
+
+Route::get('/texty', function () {
+    $articles = Article::research()->get();
+
+    // Re-order articles
+    $articles = collect([
+        $articles->firstWhere('slug', 'ztraceny-svaty'),
+        $articles->firstWhere('slug', 'cold-revolution'),
+        $articles->firstWhere('slug', 'z-vyskumu-obrazu'),
+        $articles->firstWhere('slug', 'prerusena-piesen'),
+    ])->filter();
+
+    return view('research-articles.index', compact('articles'));
+})->name('research-articles.index');
+
+Route::get('/texty/{article:slug}', function (Article $article) {
+    return view('research-articles.show', compact('article'));
+})->name('research-articles.show');
+
+Route::get('/preco', function () {
+    $articles = Article::published()->orderBy('published_at', 'desc')->take(2)->get();
+    return view('about', compact('articles'));
+})->name('about');
 
 Route::prefix('admin')->name('admin.')->middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/', function () {
@@ -40,80 +110,4 @@ Route::prefix('admin')->name('admin.')->middleware(['auth:sanctum', 'verified'])
     Route::resource('articles', ArticleController::class);
 
     Route::mediaLibrary();
-});
-
-Route::prefix('preview')->middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::get('/', function () {
-        $articles = Article::published()->orderBy('published_at', 'desc')->take(4)->get();
-        $featuredArticle = $articles->shift();
-
-        $timelapseImages = Setting::first()->getMedia('timelapse');
-        $timelapseImagesDates = $timelapseImages
-            ->map(fn ($image) => Carbon::parse($image->getCustomProperty('date')));
-
-        $researchArticles = Article::whereIn('slug', ['ztraceny-svaty', 'z-vyskumu-obrazu'])->get();
-
-        return view('welcome', compact('articles', 'featuredArticle', 'timelapseImages', 'timelapseImagesDates', 'researchArticles'));
-    })->name('home');
-
-    Route::get('/akteri', function () {
-        $articles = Article::published()->orderBy('published_at', 'desc')->take(2)->get();
-        return view('actors', compact('articles'));
-    })->name('actors');
-
-    Route::get('/pridane', function (Request $request) {
-        $articles = Article::published()->with('tags')->orderBy('published_at', 'desc');
-        $tag = $request->get('tag');
-
-        if ($tag) $articles = $articles->withAnyTags($tag);
-        $articles = $articles->get();
-
-        $comingSoon = Setting::first()->coming_soon;
-
-        return view('articles.index', compact('articles', 'tag', 'comingSoon'));
-    })->name('articles.index');
-
-    Route::get('/pridane/{article:slug}', function (Article $article) {
-        $articles = Article::published()->orderBy('published_at')->get();
-        $articleIndex = $articleIndex = $articles->search(fn ($haystack) => $haystack->id == $article->id);
-
-        // Wrap-around next and prev articles
-        $nextArticle = $articles
-            ->slice($articleIndex + 1)
-            ->first()
-            ??
-            $articles->first();
-
-        $prevArticle = $articles
-            ->slice(0, $articleIndex)
-            ->last()
-            ??
-            $articles->last();
-
-
-        return view('articles.show', compact('article', 'nextArticle', 'prevArticle'));
-    })->name('articles.show');
-
-    Route::get('/texty', function () {
-        $articles = Article::research()->get();
-
-        // Re-order articles
-        $articles = collect([
-            $articles->firstWhere('slug', 'ztraceny-svaty'),
-            $articles->firstWhere('slug', 'cold-revolution'),
-            $articles->firstWhere('slug', 'z-vyskumu-obrazu'),
-            $articles->firstWhere('slug', 'prerusena-piesen'),
-        ])->filter();
-
-        return view('research-articles.index', compact('articles'));
-    })->name('research-articles.index');
-
-    Route::get('/texty/{article:slug}', function (Article $article) {
-        return view('research-articles.show', compact('article'));
-    })->name('research-articles.show');
-
-    Route::get('/preco', function () {
-        $articles = Article::published()->orderBy('published_at', 'desc')->take(2)->get();
-        return view('about', compact('articles'));
-    })->name('about');
 });
